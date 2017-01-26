4e3009bdc31a2ebdbc02777b1a3b5ec84c1241c9
Zach Cheatham <zachcheatham@gmail.com>
2017-01-25 22:19:18 -0600

    Switch to Tkinker

--- scoreboard.py
+++ scoreboard.py
@@ -1,77 +1,25 @@
-import cairo
-import gi
 import threading
 
-gi.require_version("Gtk", "3.0")
-
-from gi.repository import Gtk, Gdk
 from RPi import GPIO
+from tkinter import Tk, Canvas, Frame, font, BOTH, CENTER
 
 MAX_SCORE = 11
 SERVES = 2
-LEFT_BUTTON = 7
-RIGHT_BUTTON = 11
 
-# Helper function draw draw text centered at a point
-def draw_text_at(cr, text, x, y, centered=False, vertical_spacing=5):
-    # Single-lined text
-    if isinstance(text, str):
-        # Get text bounds
-        text_extents = cr.text_extents(text)
+LEFT_BUTTON = 11
+RIGHT_BUTTON = 13
 
-        if centered:
-            # (In order to center vertically, we ADD half tr.he text height because
-            # cairo renders from the bottom left of the text)
-            cr.move_to(x - text_extents[2] / 2, y + text_extents[3] / 2)
-        else:
-            cr.move_to(x, y + text_extents[3])
+DEFAULT_COLOR = "#FFC900"
+OVERTIME_COLOR = "#FF0000"
+WINNING_COLOR = "#00FF00"
 
-        cr.show_text(text)
-        return (text_extents[2], text_extents[3])
-    # Multi-lined text
-    else:
-        # Get height of single line of text
-        line_height = cr.text_extents(text[0])[3]
+class Scoreboard(Frame):
+    def __init__(self, parent):
+        super(Scoreboard, self).__init__(parent)
 
-        total_width = 0 # We'll figure this as we measure lines
-        total_height = (line_height * len(text)) + (vertical_spacing * (len(text) - 1))
-
-        # Calculate the y for centering
-        starting_y = 0
-        if centered:
-            starting_y = y - total_height / 2
-        else:
-            starting_y = y
-
-        # Render each line of text
-        for i in range(0, len(text)):
-            # Get the bounds of the line
-            line_extents = cr.text_extents(text[i])
-
-            # Adjust what our total_width is
-            if line_extents[2] > total_width:
-                total_width = line_extents[2]
-
-            # Calculate the x for centering
-            render_x = 0
-            if centered:
-                render_x = x - (line_extents[2] / 2)
-            else:
-                render_x = x
-
-            # Move down
-            render_y = starting_y + line_height * i + vertical_spacing * i + line_height
-
-            # RENDER
-            cr.move_to(render_x, render_y)
-            cr.show_text(text[i])
-
-        return (total_width, total_height)
-
-
-class Scoreboard(Gtk.ApplicationWindow):
-    def __init__(self):
-        super(Scoreboard, self).__init__(title="Table Tennis Scoreboard")
+        self.parent = parent
+        self.config(bg="black", cursor='none')
+        self.bind("<Configure>", self.on_resize)
 
         self.running = True
         self.left_score = 0
@@ -82,108 +30,69 @@ class Scoreboard(Gtk.ApplicationWindow):
         self.game_over = False
         self.left_winner = False
 
-        #self.set_wmclass("Table Tennis Scoreboard", "Table Tennis Scoreboard")
-        self.fullscreen_on_monitor(Gdk.Screen.get_default(), 1)
-        self.connect("delete-event", Gtk.main_quit)
+        self.parent.title("Table Tennis Scoreboard")
+        self.pack(fill=BOTH, expand=2)
 
-        drawing_area = Gtk.DrawingArea()
-        drawing_area.connect("draw", self.on_draw)
-        self.add(drawing_area)
+        self.canvas = Canvas(self, highlightthickness=0)
+        self.canvas.configure(background="black")
+        self.canvas.pack(fill=BOTH, expand=2)
 
-        # Setup buttons
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(LEFT_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(RIGHT_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.draw_canvas()
 
-        self.left_button_thread = threading.Thread(target=self.listen_for_l_button)
-        self.left_button_thread.start()
-        self.right_button_thread = threading.Thread(target=self.listen_for_r_button)
-        self.right_button_thread.start()
+    def draw_canvas(self):
+        self.canvas.delete("all")
 
-    def listen_for_l_button(self):
-        while self.running:
-            channel = GPIO.wait_for_edge(LEFT_BUTTON, GPIO.RISING)
-            if channel == LEFT_BUTTON:
-                print ("LEFT_BUTTON")
-
-    def listen_for_r_button(self):
-        while self.running:
-            channel = GPIO.wait_for_edge(RIGHT_BUTTON, GPIO.RISING)
-            if channel == RIGHT_BUTTON:
-                print ("RIGHT_BUTTON")
-
-    def on_draw(self, widget, cr):
-        width, height = window.get_size()
+        width = self.winfo_width()
+        height = self.winfo_height()
         x_half = width / 2
         y_half = height / 2
 
-        # Background
-        cr.set_source_rgb(0, 0, 0)
-        cr.rectangle(0, 0, width, height)
-        cr.fill()
-
-        cr.select_font_face("Ozone", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
-
-        # Draw scoreboard
         if self.game_started:
+            main_color = DEFAULT_COLOR
             if not self.game_over and (self.left_score >= MAX_SCORE or self.right_score >= MAX_SCORE):
-                cr.set_source_rgb(1, 0, 0)
-            else:
-                cr.set_source_rgb(1, 0.8, 0)
-            # Center Line
-            cr.set_line_width(2)
-            cr.line_to(x_half, 0)
-            cr.line_to(x_half, height)
-            cr.stroke()
+                main_color = OVERTIME_COLOR
 
-            # Set score font
-            cr.set_font_size(height / 2)
+            self.canvas.create_line(x_half, 0, x_half, height, width=2, fill=main_color)
+
+            score_font = font.Font(family="Ozone", size=round(height/2.5))
+            label_font = font.Font(family="Ozone", size=round(height/10))
 
             # Render left score
+            color = main_color
             if self.game_over and self.left_winner:
-                cr.set_source_rgb(0, 1, 0)
-            elif not self.game_over and (self.left_score >= MAX_SCORE or self.right_score >= MAX_SCORE):
-                cr.set_source_rgb(1, 0, 0)
-            else:
-                cr.set_source_rgb(1, 0.8, 0)
-            score_width, score_height = draw_text_at(cr, '%02d' % self.left_score, x_half / 2, y_half, True)
+                color = WINNING_COLOR
+            self.canvas.create_text(x_half / 2, y_half, text='%02d' % self.left_score, fill=color, font=score_font)
 
             # Render right score
+            color = main_color
             if self.game_over and not self.left_winner:
-                cr.set_source_rgb(0, 1, 0)
-            elif not self.game_over and (self.left_score >= MAX_SCORE or self.right_score >= MAX_SCORE):
-                cr.set_source_rgb(1, 0, 0)
-            else:
-                cr.set_source_rgb(1, 0.8, 0)
-            draw_text_at(cr, '%02d' % self.right_score, x_half + x_half / 2, y_half, True)
+                color = WINNING_COLOR
+            self.canvas.create_text(x_half + x_half / 2, y_half, text='%02d' % self.right_score, fill=color, font=score_font)
 
-            score_bottom = y_half + (score_height / 2)
+            score_bottom = y_half + (round(height/2.5) / 2)
             if self.game_over:
-                # Render serving
-                cr.set_font_size(height / 10)
+                # Render winner
                 serving_position = score_bottom + ((height - score_bottom) / 2)
-                cr.set_source_rgb(0, 1, 0)
                 if self.left_winner:
-                    draw_text_at(cr, "WINNER", (x_half / 2), serving_position, True)
+                    self.canvas.create_text(x_half / 2, serving_position, text="WINNER", fill=WINNING_COLOR, font=label_font)
                 else:
-                    draw_text_at(cr, "WINNER", x_half + (x_half / 2), serving_position, True)
+                    self.canvas.create_text(x_half + x_half / 2, serving_position, text="WINNER", fill=WINNING_COLOR, font=label_font)
             else:
-                # Render serving
-                cr.set_font_size(height / 10)
+                # Render serving label
                 serving_position = score_bottom + ((height - score_bottom) / 2)
                 if self.left_serving:
-                    draw_text_at(cr, "SERVING", (x_half / 2), serving_position, True)
+                    self.canvas.create_text(x_half / 2, serving_position, text="SERVING", fill=color, font=label_font)
                 else:
-                    draw_text_at(cr, "SERVING", x_half + (x_half / 2), serving_position, True)
-
-        # Draw start instructions
+                    self.canvas.create_text(x_half + x_half / 2, serving_position, text="SERVING", fill=color, font=label_font)
         else:
-            cr.set_source_rgb(1, 0.8, 0)
-            cr.set_font_size(width / 8)
+            label_font = font.Font(family="Ozone", size=round(height/6))
+            self.canvas.create_text(x_half, y_half, text="First server\npress button\nto start", fill=DEFAULT_COLOR, font=label_font, justify=CENTER)
 
-            draw_text_at(cr, ("First server", "press button", "to start"), x_half, y_half, True, width / 14) # width 8 / 2 really means width / 14
+    def on_resize(self, event):
+        self.draw_canvas()
 
-window = Scoreboard()
-window.show_all()
-
-Gtk.main()
+root = Tk()
+root.attributes("-fullscreen", True)
+root.config(bg="black")
+window = Scoreboard(root)
+root.mainloop()
